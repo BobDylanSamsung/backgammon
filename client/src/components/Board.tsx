57b940ef2265f436ba7def7f1219a1b2652cee3a
BobDylanSamsung <dylan@herdanonline.net>
2024-08-03 15:19:32 +1000

    points

---
+++ client/src/components/Board.tsx
@@ -0,0 +1,10 @@
+import {Stack} from "@mui/material";
+import {colours} from "../common/colours.ts";
+import {ClickablePoint, Point} from "./Point.tsx";
+
+export const Board = () => (
+    // <Stack height={50} width={50} border={3} borderColor={colours.cedar} sx={{backgroundColor: colours.mahogany}}>
+    //     aldskfja;
+    // </Stack>
+    <ClickablePoint />
+)
